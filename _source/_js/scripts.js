483903f72f44ad020470a5465c72b0c3d4731d27
Luan Rieiro <luanribeiros@gmail.com>
2016-12-15 11:14:13 -0300

    add arquivos

--- _source/_js/scripts.js
+++ _source/_js/scripts.js
@@ -9,28 +9,6 @@ $(document).ready(function () {
     }
   });
 
-
-  $("a[rel^='external']").each(function () {
-    $(this).attr("target", "_blank");
-  });
-
-  // $('nav, .call-to-action').localScroll({
-  //   duration: 1000,
-  //   axis: 'y'
-  // });
-
-  // $(".fancybox").fancybox({
-  //   'width'           : 650,
-  //   'height'          : 488,
-  //   'scrolling'       : 'no',
-  //   'autoScale'       : false,
-  //   'transitionIn'    : 'none',
-  //   'transitionOut'   : 'none',
-  //   'type'            : 'iframe',
-  //   'overlayColor'    : "#000000",
-  //   'overlayOpacity'  : 0.6
-  // });
-
   $(".js-menu li").hover(
     function () {
       var link = $(this).find("a");
@@ -48,8 +26,6 @@ $(document).ready(function () {
       }, 250);
     }
   );
-
- 
 
   function colorHeader(){
     if ($(window).scrollTop() > 300 ){
@@ -80,44 +56,11 @@ $(document).ready(function () {
       // }
     });
   });
-  // $(function () {
-  //   $('a[href=\\#]').click(function (event) {
-  //     event.preventDefault();
-  //   });
-  // });
 
-   //   $("#testimonials .slider-container").jCarouselLite({
-  //     btnNext: "#testimonial-next",
-  //     btnPrev: "#testimonial-previous",
-  //     visible: 1,
-  //     speed: 1000,
-  //     beforeStart: function(e){
-  //       $(e).animate({opacity:0});
-  //       // We need both to be updated because the user can go either left or right.
-  //       $(e).prev().css({opacity: 100})
-  //       $(e).next().css({opacity: 100})
-  //     }
-  //   });
-
-  //   var $c = $('#developed-projects-slider ul');
-  //   while ($c.children('div').length) {
-  //     $c.children('div:lt(15)').wrapAll('<li>');
-  //   }
-
-  //   $("#developed-projects-slider ul div:nth-child(5)").addClass("last")
-  //   $("#developed-projects-slider ul div:nth-child(10)").addClass("last")
-  //   $("#developed-projects-slider ul div:nth-child(15)").addClass("last")
-
-  //   $("#projects-slider-navigation a").click(function(){
-  //     $("#projects-slider-navigation a").removeClass("selected");
-  //     $(this).addClass("selected");
-  //   });
-
-  //   $("#developed-projects-slider div").jCarouselLite({
-  //       btnNext: "#slider-next",
-  //       btnPrev: "#slider-previous",
-  //       visible: 1,
-  //       btnGo: ["#projects-slider-navigation .1", "#projects-slider-navigation .2", "#projects-slider-navigation .3"]
-  //   });
+  $(function () {
+    $('a').click(function (event) {
+      event.preventDefault();
+    });
+  });
 
 });
